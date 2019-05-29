738a067a8cb15c89a8a33d306b6b182f77b4d2d1
Aaronsom <Aaronsom@users.noreply.github.com>
2019-05-29 18:41:31 +0200

    updated global constants for colab

--- poem_generator/global_constants.py
+++ poem_generator/global_constants.py
@@ -4,9 +4,9 @@ OUT_OF_VOCAB_TOKEN = "<oov/>"
 START_OF_SEQUENCE_TOKEN = "<sos/>"
 PADDING_TOKEN = "<padding/>"
 
-TRAINING_DATA = r"C:\Users\Gregor\Documents\Programming\poem-generation\data\train_poems.txt"
+TRAINING_DATA = r"data/train_poems.txt"
 
-MODELS_DICT = r"C:\Users\Gregor\Documents\Programming\poem-generation\models"
+MODELS_DICT = r"models"
 
 EMBEDDING_PATH = r"C:\Users\Gregor\Documents\Programming\embedding\GoogleNews-vectors-negative300.bin"
 EMBEDDING_DIMENSION = 300
